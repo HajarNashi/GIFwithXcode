731dad1432d1444907e2119ef9cac60339bebccf
HajarNashi <HajarNashi@gmail.com>
2022-12-15 20:53:20 +0300

    Initial Commit

---
+++ GIF/GIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFApp.swift
+//  GIF
+//
+//  Created by Hajar Nashi on 21/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
